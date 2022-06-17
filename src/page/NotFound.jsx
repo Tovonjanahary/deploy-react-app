18dfa4a10e44d436872cc9b35f63380588848738
Tovonjanahary <tovonjanahary06@gmail.com>
2022-06-17 16:24:48 +0300

    config2

--- src/page/NotFound.jsx
+++ src/page/NotFound.jsx
@@ -7,7 +7,7 @@ const NotFound = () => {
             <div className="w-200 h-200 md:w-200 mx-auto py-6 text-center">
              <img className="w-full md:w-1/2 lg:mx-auto md:mx-auto z-50" alt="not-found" src="../img/undraw_page_not_found_su7k.png" />
              <p className="leading-normal text-2xl mb-8">
-                        Desolé, nous n'avons pas trouvé la page demandée
+                        Desolé, nous n'avons pas trouvé la page demandé
 
              </p>
              <Link to="/" className="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
