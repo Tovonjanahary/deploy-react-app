02c34022082171026c76f7ad901dda5b4d56dba9
Tovonjanahary <tovonjanahary06@gmail.com>
2022-06-19 23:36:26 +0300

    cofing port

--- back/server.js
+++ back/server.js
@@ -31,7 +31,7 @@ if(process.env.NODE_ENV === "production") {
   })
 }
 
-const port = process.env.PORT || 5000;
-app.listen(port, () => {
+// const port = process.env.PORT || 5000;
+app.listen(5000, () => {
   console.log("Server is up on port " + port);
 });
